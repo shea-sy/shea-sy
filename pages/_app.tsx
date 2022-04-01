f647dba9c3d320c22e7ee7ca1e03c536dd724f34
shea-sy <shea-sy@qq.com>
2022-04-02 00:20:05 +0800

    Prism配色

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,10 @@ import 'styles/global.css'
 import 'react-notion-x/src/styles.css'
 
 // used for code syntax highlighting (optional)
-// import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/themes/prism-coy.css'
 
 // this might be better for dark mode
-import 'prismjs/themes/prism-okaidia.css'
+// import 'prismjs/themes/prism-okaidia.css'
 
 // used for collection views selector (optional)
 import 'rc-dropdown/assets/index.css'
